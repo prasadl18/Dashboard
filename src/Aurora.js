0a3afda071bdda73ee66e832e7e34f428f1bc1bb
Prasad <prasadlimbekar21@gmail.com>
2025-09-08 17:41:41 +0530

    Fix eslint react-hooks error and install plugin

--- src/Aurora.js
+++ src/Aurora.js
@@ -195,7 +195,7 @@ export default function Aurora(props) {
       }
       gl.getExtension('WEBGL_lose_context')?.loseContext();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    
   }, [amplitude]);
 
   return <div ref={ctnDom} className="aurora-container" />;
